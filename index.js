6ad3591fe1221b229740459c2edacec794fa699e
Holandess <felipe.holandes17@gmail.com>
2018-10-25 16:12:53 +0000

    integracao da planilha funcionando

--- index.js
+++ index.js
@@ -5,23 +5,17 @@
 const functions = require('firebase-functions');
 
 const { WebhookClient } = require('dialogflow-fulfillment');
-const { Card, Suggestion } = require('dialogflow-fulfillment');
-const firebase = require("firebase");
+
 const request = require('request');
 
 // init firestore
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
-var db = admin.firestore();
+// init fb
 
-// init fb   
 var FB = require('fb');
 FB.options({ Promise: Promise });
-
-
-FB.setAccessToken('');
-
-
+FB.setAccessToken('aqui fica seu token de acesso');
 
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
@@ -33,15 +27,12 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 
 
   intentMap.set('boasVindas', boasVindas);
-
   agent.handleRequest(intentMap);
 });
 
-
 function boasVindas(agent) {
   //console.log(JSON.stringify(agent.originalRequest.payload.data.sender));
-
-
+  
   let platformPayload = agent.originalRequest.payload;
   let senderId;
   let nome;
@@ -50,37 +41,24 @@ function boasVindas(agent) {
     senderId = platformPayload.data.sender.id;
     return FB.api(senderId, 'GET').then((response) => {
       nome = response.name;
+      agent.add('Ola '+nome+' tudo bem? ');
+      
+      var options = { method: 'POST',
+ url: 'https://script.google.com/macros/s/AKfycbzhuv1-iXcfcDH8zeVlPbfurhZo4Q01wY_uUsYmvuBELmaTl0E/exec',
+ headers: 
+  {'cache-control': 'no-cache',
+    'content-type': 'application/json' },
+ body: { row: nome},
+ json: true };
 
-      agent.add('Olá' + nome);
+request(options, function (error, response, body) {
+ if (error) throw new Error(error);
 
-      var options = {
-        method: 'POST',
-        url: 'https://script.google.com/macros/s/AKfycbzhuv1-iXcfcDH8zeVlPbfurhZo4Q01wY_uUsYmvuBELmaTl0E/exec',
-        body: {
-          row: nome
-        },
-
-        headers: {
-          'cache-control': 'no-cache'
-        }
-
-      };
-      request(options, function(error, response, body) {
-        if (error) throw new Error(error);
-        console.log(body);
-        let ret = JSON.parse(body);
-        console.log(ret);
-        
-      });
+ console.log(body);
+}); 
     }).catch((error) => {
       agent.add("Desculpe, não sei seu nome :( ");
 
     });
   
 }}
-
-// Get the database collection 'dialogflow' and document 'agent' and store
-// the document  {entry: "<value of database entry>"} in the 'agent' document
-
-
-//firebase deploy --only functions
