a907b4088124b3cc259b568a933ee005ba45841b
Holandess <felipe.holandes17@gmail.com>
2018-10-29 17:39:29 +0000

    Integracao com planilhas funcionando

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 // See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
 'use strict';
 
-
 const functions = require('firebase-functions');
 
-const { WebhookClient } = require('dialogflow-fulfillment');
-
+const { WebhookClient, Image } = require('dialogflow-fulfillment');
+const { Card, Suggestion } = require('dialogflow-fulfillment');
+const firebase = require("firebase");
 const request = require('request');
+const { google } = require('googleapis');
 
 // init firestore
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
-// init fb
+// IMPORTAÇÃO DO REQUEST E DATATIME
+// const requests = require('requests');
+// const datetime = require('datetime');
 
+// init fb   
 var FB = require('fb');
 FB.options({ Promise: Promise });
-FB.setAccessToken('aqui fica seu token de acesso');
+
+
+FB.setAccessToken('DQVJzMGZATTWdPTkVCRDJTYXJWRTBhcGVwUmFJM1dRVkthbnplZAmp3cWdaVFo3UjEzbUV3VkpsQlBldWpGYTMxS3plTmFGa1F4Q2VDMzBqODhqUjdsNndvLTU2MEhEMnhVd2VKNUk4aGJwRlpQN01WWmJTX2xVSXlJWGMxMFNfUlFjTUpEcU41RGVHZAE5CMjdFVkNyMTJrNmNFZAXBzU25Mbll1UUxYaU5HbFJDYTdSOGtRQ0YxLTZAYSzVSMXZApTFduMTRBXzYxcTdGMU4tbUt1WgZDZD');
+
 
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
@@ -23,42 +30,109 @@ process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
 
   const agent = new WebhookClient({ request, response });
+
   let intentMap = new Map();
-
-
-  intentMap.set('boasVindas', boasVindas);
+  intentMap.set('Mapa inicial.1-Começar', mapaInicial);
+  intentMap.set('Conversa Final.3-Like ou Deslike', enviarPlanilha);
   agent.handleRequest(intentMap);
 });
 
-function boasVindas(agent) {
+
+
+function mapaInicial(agent) {
   //console.log(JSON.stringify(agent.originalRequest.payload.data.sender));
-  
+
+
   let platformPayload = agent.originalRequest.payload;
   let senderId;
   let nome;
+  let nomeSplit;
+
+  /* Em caso de necessidade imagem da assistente.  
+  let image = new Image('https://storage.googleapis.com/marista-chatbot.appspot.com/avatarDhoraApresentacao.jpg'); 
+  */
 
   if (platformPayload.source == "facebook") {
     senderId = platformPayload.data.sender.id;
     return FB.api(senderId, 'GET').then((response) => {
+
       nome = response.name;
-      agent.add('Ola '+nome+' tudo bem? ');
-      
-      var options = { method: 'POST',
- url: 'https://script.google.com/macros/s/AKfycbzhuv1-iXcfcDH8zeVlPbfurhZo4Q01wY_uUsYmvuBELmaTl0E/exec',
- headers: 
-  {'cache-control': 'no-cache',
-    'content-type': 'application/json' },
- body: { row: nome},
- json: true };
+      nomeSplit = nome.split(" ", 1);
 
-request(options, function (error, response, body) {
- if (error) throw new Error(error);
-
- console.log(body);
-}); 
+      agent.add('Olá ' + nomeSplit + ', tudo bem?');
+      agent.add('Eu sou a Dhora, consultora virtual da DDHO. Hoje eu gostaria de falar com você sobre a Pesquisa de Engajamento do Grupo Marista. Se você puder agora, clique em começar e vamos lá');
     }).catch((error) => {
       agent.add("Desculpe, não sei seu nome :( ");
 
     });
-  
-}}
+
+  }
+}
+
+function enviarPlanilha(agent) {
+  let con = agent.contexts;
+  console.log(con);
+  let param;
+  for (let i = 0; i < con.length; i++) {
+    if (con[i].name === 'master-followup') {
+      param = con[i].parameters;
+      console.log("Contexto " + con[i].name + ":" + con[i]);
+    }
+  }
+  console.log("Parameters:" + param);
+
+  let arrayParam = [
+    param.frenteDeMissao,
+    param.diretoriaArea,
+    param.simNaoResultadosPesquisas,
+    param.gestorCompartilhouDados,
+    param.gestorApresentouPontos,
+    param.oQueSeraFeitoResultados,
+    param.likeDeslike
+  ];
+  console.log("Array of Parameters without treatment:" + arrayParam);
+
+  for (let i = 0; i < arrayParam.length; i++) {
+    if (typeof arrayParam[i] == "undefined") {
+      arrayParam[i] = '';
+    }
+  }
+
+  console.log("arrayParam with treatment" + arrayParam);
+
+  sendToSheet(arrayParam, agent);
+}
+
+function sendToSheet(array, agent) {
+  let platformPayload = agent.originalRequest.payload;
+  let senderId;
+
+  if (platformPayload.source == "facebook") {
+    senderId = platformPayload.data.sender.id;
+    return FB.api(senderId, 'GET').then((response) => {
+      array.unshift(senderId, response.name);
+
+      var options = {
+        method: 'POST',
+        url: 'https://script.google.com/macros/s/AKfycbzyGebRp48PS7zLXhfb2vxX5MFHxoy088sz3063ISpU8ZJYMSQ/exec',
+        json: true,
+        body: {
+          input: array
+        },
+
+        headers: {
+          'cache-control': 'no-cache'
+        }
+      };
+      request(options, function(error, response, body) {
+        if (error) throw new Error(error);
+        console.log(body);
+        let ret = JSON.parse(body);
+        console.log(ret);
+        console.log(error);
+      });
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+}
